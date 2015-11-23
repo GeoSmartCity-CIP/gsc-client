58a8ca7035853e6101f08b2b9896f5b320548de0
Eirik Csák Knutsen <eck@avinet.no>
2015-11-23 15:35:59 +0100

    Configure grunt
    Configure grunt, enforce styling rules and code checker

---
+++ src/gsc.js
@@ -0,0 +1,25 @@
+/**
+ * <p>Geosmartcity client-side library for rapid application development based
+ * on the GSC platform OpenAPI</p>
+ * <p>Functions to draw statistical maps for use with OpenLayers based
+ * applications.</p>
+ *
+ * @namespace
+ * @requires d3.v3
+ * @requires jQuery-1.10.2
+ * @requires jQuery-xml2json
+ * @requires jQuery-number-2.1.3
+ * @requires topojson
+ * @requires simple-statistics
+ * @requires queue.v1
+ * @requires topojson.v1
+ */
+
+var gsc = {
+  /**
+   * Version number of the s4a.js library
+   * @type Number
+   */
+  version: '0.1.0'
+};
+
