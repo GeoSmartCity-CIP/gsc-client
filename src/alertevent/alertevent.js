e05acac7c291a6ed87a0094f85ddf5ea9e94a278
filipzava <filip.zavadil@mapshakers.com>
2016-02-03 15:31:14 +0100

    tests

--- src/alertevent/alertevent.js
+++ src/alertevent/alertevent.js
@@ -3,6 +3,8 @@
  * @namespace gsc
  */
 
+var gsc = gsc || {};
+
 gsc.alertevent = {};
 
 /**
@@ -13,100 +15,96 @@ gsc.alertevent = {};
  */
 
 gsc.alertevent.AE = function(url) {
-    var _self = this;
-    url = url || 'http://geo.mapshakers.com:8080/CrowsSourcing';
+  var _self = this;
+  url = url || 'http://geo.mapshakers.com:8080/CrowsSourcing';
 
+  /**
+   * Recieve a config file
+   *
+   * @param {Function} callback The calback function f(error,data)
+   */
+  _self.getConfig = function(callback) {
 
-    /**
-     * Recieve a config file
-     *
-     * @param {Function} callback The calback function f(error,data)
-     */
-    _self.getConfig = function(callback){
-
-        $.get(url + '/config')
-            .done(function( data ) {
-                console.log( data );
+    $.get(url + '/config')
+            .done(function(data) {
+              callback(data);
             })
-            .fail(function( err ) {
-                console.error( err );
+            .fail(function(err) {
+              callback(err);
             });
 
-    };
+  };
 
-    /**
-     * Create a comment
-     *
-     * @param {Function} callback The calback function f(error,data)
-     * @param {JSON} data The JSON data object
-     * @param {String} uuid Identification string of event
-     */
-    _self.eventComment =  function(callback,data,uuid){
+  /**
+   * Create a comment
+   *
+   * @param {Function} callback The calback function f(error,data)
+   * @param {JSON} data The JSON data object
+   * @param {String} uuid Identification string of event
+   */
+  _self.eventComment =  function(callback, data, uuid) {
 
-
-        $.ajax({
-            url: url + '/event/comment/'+uuid,
+    $.ajax({
+            url: url + '/event/comment/' + uuid,
             type: 'POST',
             data: JSON.stringify(data),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'}
-            )
-            .done(function( data ) {
-                console.log( data );
+    )
+            .done(function(data) {
+              callback(data);
             })
-            .fail(function( err ) {
-                console.error( err );
+            .fail(function(err) {
+              callback(err);
             });
-    };
+  };
 
-    /**
-     * Create an event
-     *
-     * @param {Function} callback The calback function f(error,data)
-     * @param {FormData} formdata The FormData object (JSON + attachment)
-     */
-    _self.eventCreate =  function(callback,formdata){
+  /**
+   * Create an event
+   *
+   * @param {Function} callback The calback function f(error,data)
+   * @param {FormData} formdata The FormData object (JSON + attachment)
+   */
+  _self.eventCreate =  function(callback, formdata) {
 
-       $.ajax({
-                url: url + '/event/create',
-                type: 'POST',
-                data: formdata,
-                processData : false,
-                contentType : false
-            }
-        )
-            .done(function( data ) {
-                console.log( data );
+    $.ajax({
+      url: url + '/event/create',
+      type: 'POST',
+      data: formdata,
+      processData: false,
+      contentType: false
+    }
+)
+            .done(function(data) {
+              callback(data);
             })
-            .fail(function( err ) {
-                console.error( err );
+            .fail(function(err) {
+              callback(err);
             });
-    };
+  };
 
+  /**
+   * Filter list on bounding box
+   **
+   * @param {Function} callback The calback function f(error,data)
+   * @param {JSON} data The JSON data object
+   */
+  _self.eventListFilter =  function(callback, data) {
 
-    /**
-     * Filter list on bounding box
-     **
-     * @param {Function} callback The calback function f(error,data)
-     * @param {JSON} data The JSON data object
-     */
-    _self.eventListFilter =  function(callback,data){
-
-
-        $.ajax({
-                url: url + '/event/list',
-                type: 'POST',
-                data: JSON.stringify(data),
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json'}
-        )
-            .done(function( data ) {
-                console.log( data );
+    $.ajax({
+            url: url + '/event/list',
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'}
+    )
+            .done(function(data) {
+              callback(data);
             })
-            .fail(function( err ) {
-                console.error( err );
+            .fail(function(err) {
+              callback(err);
             });
-    };
+  };
 
-    return _self;
+  return _self;
 };
