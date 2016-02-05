2e04814ab69c15cdf753b3a48596d6ed09632aee
filipzava <filip.zavadil@mapshakers.com>
2016-02-06 00:47:36 +0100

    new API

--- src/cs/cs.js
+++ src/cs/cs.js
@@ -1,110 +1,126 @@
 /**
+ * <p>API for crowd sourcing feature of gsc.js library</p>
+ * <p>Functions to handle server side crowd sourcing app.</p>
  *
- * @namespace gsc
+ * @namespace gsc.cs
+ * @requires jQuery-2.1.4
  */
+
 
 var gsc = gsc || {};
 
-gsc.cs = {};
+gsc.cs = {
+    /**
+     * Version number of the crowd sourcing feauture of gsc.js
+     * @type {Number}
+     */
+    version: '0.1.0',
+    /**
+     * URI of server-side crowd sourcing application
+     * @type {String}
+     */
+    url: 'http://geo.mapshakers.com:8080/CrowsSourcing'
+};
 
 /**
  * Create an alert event object
  *
  * @constructor
- * @param {String} url The url of an aplication
+ * @param {String} url The url of an application
  */
 
-gsc.cs.AE = (function(url) {
-  var _self = this;
-  url = url || 'http://geo.mapshakers.com:8080/CrowsSourcing';
 
-  /**
-   * Recieve a config file
-   *
-   * @param {Function} callback The calback function f(error,data)
-   */
-  _self.getConfig = function(callback) {
+/**
+ * Receive a config file
+ *
+ * @param {Function} callback The calback function f(error,data)
+ */
+gsc.cs.getConfig = function(callback) {
 
-    $.get(url + '/config')
-            .done(function(data) {
-              callback(data);
-            })
-            .fail(function(err) {
-              callback(err);
-            });
+    $.get(gsc.cs.url + '/config')
+        .done(function(data) {
+            callback(null,data);
+        })
+        .fail(function(err) {
+            callback(err);
+        });
 
-  };
+};
 
-  /**
-   * Create a comment
-   *
-   * @param {Function} callback The calback function f(error,data)
-   * @param {JSON} data The JSON data object
-   * @param {String} uuid Identification string of event
-   */
-  _self.eventComment =  function(callback, data, uuid) {
+/**
+ * Create a comment
+ *
+ * @param {requestCallback} cb - The callback that handles the response.
+ * @param {JSON} data The JSON data object
+ * @param {String} uuid Identification string of event
+ */
+gsc.cs.eventComment =  function(cb, data, uuid) {
 
     $.ajax({
-            url: url + '/event/comment/' + uuid,
+            url: gsc.cs.url + '/event/comment/' + uuid,
             type: 'POST',
             data: JSON.stringify(data),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'}
     )
-            .done(function(data) {
-              callback(data);
-            })
-            .fail(function(err) {
-              callback(err);
-            });
-  };
+        .done(function(data) {
+            cb(null,data);
+        })
+        .fail(function(err) {
+            cb(err);
+        });
+};
 
-  /**
-   * Create an event
-   *
-   * @param {Function} callback The calback function f(error,data)
-   * @param {FormData} formdata The FormData object (JSON + attachment)
-   */
-  _self.eventCreate =  function(callback, formdata) {
-
-    $.ajax({
-      url: url + '/event/create',
-      type: 'POST',
-      data: formdata,
-      processData: false,
-      contentType: false
-    }
-)
-            .done(function(data) {
-              callback(data);
-            })
-            .fail(function(err) {
-              callback(err);
-            });
-  };
-
-  /**
-   * Filter list on bounding box
-   **
-   * @param {Function} callback The calback function f(error,data)
-   * @param {JSON} data The JSON data object
-   */
-  _self.eventListFilter =  function(callback, data) {
+/**
+ * Create an event
+ *
+ * @param {requestCallback} cb - The callback that handles the response.
+ * @param {FormData} formdata The FormData object (JSON + attachment)
+ */
+gsc.cs.eventCreate =  function(cb, formdata) {
 
     $.ajax({
-            url: url + '/event/list',
+            url: gsc.cs.url + '/event/create',
+            type: 'POST',
+            data: formdata,
+            processData: false,
+            contentType: false
+        }
+    )
+        .done(function(data) {
+            cb(null,data);
+        })
+        .fail(function(err) {
+            cb(err);
+        });
+};
+
+/**
+ * Filter list on bounding box
+ **
+ * @param {requestCallback} cb - The callback that handles the response.
+ * @param {JSON} data The JSON data object
+ */
+gsc.cs.eventListFilter =  function(cb, data) {
+
+    $.ajax({
+            url: gsc.cs.url + '/event/list',
             type: 'POST',
             data: JSON.stringify(data),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'}
     )
-            .done(function(data) {
-              callback(data);
-            })
-            .fail(function(err) {
-              callback(err);
-            });
-  };
+        .done(function(data) {
+            cb(null,data);
+        })
+        .fail(function(err) {
+            cb(err);
+        });
+};
 
-  return _self;
-})();
+/**
+ * This callback is displayed as a global member.
+ * @callback requestCallback
+ * @param {object} error An error object received from server
+ * @param {object} responseData A data object (JSON)
+ */
