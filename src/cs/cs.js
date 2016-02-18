d3e00d645d288bf186beaf7194c4864e978fa87e
filipzava <filip.zavadil@mapshakers.com>
2016-02-18 09:48:46 +0100

    crows fix

--- src/cs/cs.js
+++ src/cs/cs.js
@@ -6,19 +6,20 @@
  * @requires jQuery-2.1.4
  */
 
+
 var gsc = gsc || {};
 
 gsc.cs = {
-  /**
-   * Version number of the crowd sourcing feauture of gsc.js
-   * @type {Number}
-   */
-  version: '0.1.0',
-  /**
-   * URI of server-side crowd sourcing application
-   * @type {String}
-   */
-  url: 'http://geo.mapshakers.com:8080/CrowsSourcing'
+    /**
+     * Version number of the crowd sourcing feauture of gsc.js
+     * @type {Number}
+     */
+    version: '0.1.0',
+    /**
+     * URI of server-side crowd sourcing application
+     * @type {String}
+     */
+    url: 'http://geo.mapshakers.com:8080/CrowdSourcing'
 };
 
 /**
@@ -28,19 +29,20 @@ gsc.cs = {
  * @param {String} url The url of an application
  */
 
+
 /**
  * Receive a config file
  *
- * @param {Function} callback The calback function f(error,data)
+ * @param {Function} callback The callback function f(error,data)
  */
 gsc.cs.getConfig = function(callback) {
 
-  $.get(gsc.cs.url + '/config')
+    $.get(gsc.cs.url + '/config')
         .done(function(data) {
-          callback(null,data);
+            callback(null,data);
         })
         .fail(function(err) {
-          callback(err);
+            callback(err);
         });
 
 };
@@ -54,18 +56,18 @@ gsc.cs.getConfig = function(callback) {
  */
 gsc.cs.eventComment =  function(cb, data, uuid) {
 
-  $.ajax({
-          url: gsc.cs.url + '/event/comment/' + uuid,
-          type: 'POST',
-          data: JSON.stringify(data),
-          contentType: 'application/json; charset=utf-8',
-          dataType: 'json'}
-  )
+    $.ajax({
+            url: gsc.cs.url + '/event/comment/' + uuid,
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'}
+    )
         .done(function(data) {
-          cb(null,data);
+            cb(null,data);
         })
         .fail(function(err) {
-          cb(err);
+            cb(err);
         });
 };
 
@@ -77,19 +79,19 @@ gsc.cs.eventComment =  function(cb, data, uuid) {
  */
 gsc.cs.eventCreate =  function(cb, formdata) {
 
-  $.ajax({
-    url: gsc.cs.url + '/event/create',
-    type: 'POST',
-    data: formdata,
-    processData: false,
-    contentType: false
-  }
-)
+    $.ajax({
+            url: gsc.cs.url + '/event/create',
+            type: 'POST',
+            data: formdata,
+            processData: false,
+            contentType: false
+        }
+    )
         .done(function(data) {
-          cb(null,data);
+            cb(null,data);
         })
         .fail(function(err) {
-          cb(err);
+            cb(err);
         });
 };
 
@@ -101,20 +103,21 @@ gsc.cs.eventCreate =  function(cb, formdata) {
  */
 gsc.cs.eventListFilter =  function(cb, data) {
 
-  $.ajax({
-          url: gsc.cs.url + '/event/list',
-          type: 'POST',
-          data: JSON.stringify(data),
-          contentType: 'application/json; charset=utf-8',
-          dataType: 'json'}
-  )
+    $.ajax({
+            url: gsc.cs.url + '/event/list',
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'}
+    )
         .done(function(data) {
-          cb(null,data);
+            cb(null,data);
         })
         .fail(function(err) {
-          cb(err);
+            cb(err);
         });
 };
+
 /**
  * This callback is displayed as a global member.
  * @callback requestCallback
