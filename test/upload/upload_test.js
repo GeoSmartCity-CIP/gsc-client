c37942727c2516cceffa0a03a7c35d0ba6162a9f
Adam Kaput <adamkaput@gmail.com>
2016-03-08 16:11:02 +0100

    upload test alpha

--- test/upload/upload_test.js
+++ test/upload/upload_test.js
@@ -1,1 +1,14 @@
-//some nodeunit tests
+var gsc = require('../../src/upload/upload');
+// Only for test purposes
+var FileAPI = require('file-api');
+var Node = require('bufferjs');
+var File = FileAPI.File;
+var exports = exports || {};
+
+exports.testuje = function(test) {
+  var file = new File();
+  var asd = new gsc.upload.Data(file, 20);
+  asd.isFileTypeCorrect().expect(1);
+  test.ok(true, 'this assertion should pass');
+  test.done();
+};
