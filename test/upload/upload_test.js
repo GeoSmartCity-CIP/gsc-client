0689e3145f38a85ed351bb0c434bee37bab84e6d
Adam Kaput <adamkaput@gmail.com>
2016-03-09 02:18:40 +0100

    Working on tests

--- test/upload/upload_test.js
+++ test/upload/upload_test.js
@@ -1,14 +1,12 @@
 var gsc = require('../../src/upload/upload');
 // Only for test purposes
 var FileAPI = require('file-api');
-var Node = require('bufferjs');
 var File = FileAPI.File;
 var exports = exports || {};
 
 exports.testuje = function(test) {
-  var file = new File();
-  var asd = new gsc.upload.Data(file, 20);
-  asd.isFileTypeCorrect().expect(1);
-  test.ok(true, 'this assertion should pass');
+  var file = new File('test.geojson');
+  var asd = new gsc.upload.Data(file);
+  test.ok(asd.isFileTypeCorrect(), 'this assertion should pass');
   test.done();
 };
