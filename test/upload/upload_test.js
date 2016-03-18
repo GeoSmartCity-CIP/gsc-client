847c8fa1ac24585a2c7621e0b077c063a9a2a48b
Adam Kaput <adamkaput@gmail.com>
2016-03-18 16:39:27 +0100

    Fixes, tests

--- test/upload/upload_test.js
+++ test/upload/upload_test.js
@@ -2,11 +2,54 @@ var gsc = require('../../src/upload/upload');
 // Only for test purposes
 var FileAPI = require('file-api');
 var File = FileAPI.File;
-var exports = exports || {};
 
-exports.isFileTypeCorrect = function(test) {
+exports.isGeojsonCorrect = function(test) {
   var file = new File('test.geojson');
-  var asd = new gsc.upload.Data(file);
-  test.ok(asd.isFileTypeCorrect(), 'this assertion should pass');
+  var dataObject = new gsc.upload.Data(file);
+  test.ok(dataObject.isFileTypeCorrect());
+  test.done();
+};
+exports.isShpCorrect = function(test) {
+  var file = new File('test.zip');
+  var dataObject = new gsc.upload.Data(file);
+  test.ok(dataObject.isFileTypeCorrect());
+  test.done();
+};
+exports.isGmlCorrect = function(test) {
+  var file = new File('test.gml');
+  var dataObject = new gsc.upload.Data(file);
+  test.ok(dataObject.isFileTypeCorrect());
+  test.done();
+};
+exports.iskmlCorrect = function(test) {
+  var file = new File('test.kml');
+  var dataObject = new gsc.upload.Data(file);
+  test.ok(dataObject.isFileTypeCorrect());
+  test.done();
+};
+exports.iskmlCorrect = function(test) {
+  var file = new File('test.kml');
+  var dataObject = new gsc.upload.Data(file);
+  test.ok(dataObject.isFileTypeCorrect());
+  test.done();
+};
+exports.iskmlCorrect = function(test) {
+  var file = new File('test.kml');
+  var dataObject = new gsc.upload.Data(file);
+  test.ok(dataObject.isFileTypeCorrect());
+  test.done();
+};
+exports.isEmptyFileFalse = function(test) {
+  var file = new File({name: 'filename', type: 'text/plain', buffer: new Buffer('')});
+  var dataObject = new gsc.upload.Data(file);
+  test.strictEqual(dataObject.isFileTypeCorrect(), false);
+  test.ok(dataObject.isFileSizeCorrect());
+  test.done();
+};
+exports.isTooBigFileFalse = function(test) {
+  var file = new File({name: 'filename', type: 'text/plain', buffer: new Buffer(''), size: 10000000});
+  var dataObject = new gsc.upload.Data(file);
+  test.strictEqual(dataObject.isFileTypeCorrect(), false);
+  test.ok(dataObject.isFileSizeCorrect());
   test.done();
 };
