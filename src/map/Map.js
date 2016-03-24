d5f850d0e1b282a7e3286072dc5f9eb97b6f9de8
AndreaDiNora <andrea.dinora@sinergis.it>
2016-03-24 17:26:07 +0100

    Added map code

---
+++ src/map/Map.js
@@ -0,0 +1,100 @@
+var gsc = gsc || {};
+
+gsc.map = gsc.map || {};
+
+/**
+ * Create a map with standard functions (pan,zoom, view layers) and
+ * other optional  specific functionality as info on feature and filter
+ * on attributes
+ *
+ * @param {divObject} divObject is the map div
+ * @param {mapOptions} mapOptions are the map options
+ * @constructor
+ */
+gsc.map.Map = function(divObject, mapOptions) {
+  var _self = this;
+  var layers = mapOptions.layers;
+
+  var projection = new ol.proj.Projection({
+    code: mapOptions.epsg,
+    units: mapOptions.units
+  });
+
+  var olMap = new ol.Map({
+    controls: ol.control.defaults({
+          attribution: false
+        }),
+    layers: layers,
+    target: divObject,
+    view: new ol.View({
+      projection: projection
+    })
+  });
+
+  _self.addLayer = function(layer) {
+    olMap.addLayer(layer);
+    layers.push(layer);
+  };
+
+  _self.getDomElement = function() {
+    return $(olMap.getViewport());
+  };
+
+  _self.getOlMap = function() {
+    return olMap;
+  };
+
+  _self.redraw = function() {
+    jQuery.each(layers, function(position, layer) {
+      layer.redraw();
+    });
+  };
+
+  _self.removeLayer = function(layer) {
+    olMap.removeLayer(layer);
+    layers = layers.filter(function(value) {
+      return value !== layer;
+    });
+  };
+
+  _self.fit = function(bounds) {
+    olMap.getView().fit(bounds, olMap.getSize());
+  };
+
+  _self.addMousePositionControl = function(location) {
+    var mousePositionControl = new ol.control.MousePosition({
+        className: 'custom-mouse-position',
+        target: document.getElementById(location),
+        coordinateFormat: ol.coordinate.createStringXY(5),
+        undefinedHTML: '&nbsp;'
+      });
+    olMap.addControl(mousePositionControl);
+  };
+
+  _self.addScaleBarControl = function(scalediv) {
+    olMap.getView().on('change:resolution', function(evt) {
+      var resolution = evt.target.get('resolution');
+      var units = olMap.getView().getProjection().getUnits();
+      var dpi = 25.4 / 0.28;
+      var mpu = ol.proj.METERS_PER_UNIT[units];
+      var scale = resolution * mpu * 39.37 * dpi;
+      if (scale >= 9500 && scale <= 950000) {
+        scale = Math.round(scale / 1000) + 'K';
+      } else if (scale >= 950000) {
+        scale = Math.round(scale / 1000000) + 'M';
+      } else {
+        scale = Math.round(scale);
+      }
+      document.getElementById(scalediv).innerHTML = 'Scale = 1 : ' + scale;
+    });
+    //fire change resolution event and restore previous configuration
+    olMap.getView().setZoom(olMap.getView().getZoom() + 1);
+    olMap.getView().setZoom(olMap.getView().getZoom() - 1);
+  };
+
+  if (mapOptions.bounds) {
+    olMap.getView().fit(mapOptions.bounds, olMap.getSize());
+  }
+
+  return _self;
+};
