3115c8b3637feb89dd34d3562f71bb80c9c7e4f0
Stein Runar Bergheim <runar@bergheim.dk>
2016-03-28 22:09:29 +0200

    - Added methods for calling datasources, users ws - Added data catalogue config URL etc. to gsc root object - Added browser tests for datasource

--- src/gsc.js
+++ src/gsc.js
@@ -1,22 +1,84 @@
-/*global module: true*/
-var module = module || {};
+'use strict';
 
-/**
- * <p>Geosmartcity client-side library for rapid application development based
- * on the GSC platform OpenAPI</p>
- * <p>Functions to draw statistical maps for use with OpenLayers based
- * applications.</p>
- *
- * @namespace
- * @requires jQuery-2.1.4
+var gsc = (function() {
+
+  /**
+   * Version number
+   *
+   * @type {String}
+   * @private
  * @requires OpenLayers-3.13.1
- */
+   */
+  var _version = '0.1.0';
 
-var gsc = module.exports = {
+  /**
+   * URL of the GSC Datacatalog instance the API is working against
+   * @type {String}
+   * @private
+   */
+  var _dcUrl = 'http://hub.geosmartcity.eu/' +
+    'gsc-datacatalogue/datacatalogservlet';
+
+  /**
+   * 'gsc' is the root object of the gsc.js library and the only variable to
+   * be introduced into the global namespace.
+   *
+   * @exports gsc
+   */
+  var mod = {};
+
   /**
    * Version number of the s4a.js library
-   * @type Number
+   * @type {Number}
    */
-  version: '0.1.0'
-};
+  mod.version = _version;
 
+  /**
+   * Get or set GSC Datacatalog URL. If a parameter is supplied,
+   * it is assumed to be a valid URL to the web service end-point of
+   * a GSC Datacatalogue instance.
+   *
+   * If no parameter is provided, the function will return the currently
+   * configured URL.
+   *
+   * @param {String} [dcUrl] URL to working instance of GSC Datacatalogue
+   * @return {String} URL of GSC Datacatalogue instance web service end-point
+   */
+  mod.dcUrl = function(dcUrl) {
+    if (dcUrl !== undefined) {
+      _dcUrl = dcUrl;
+    }
+    return _dcUrl;
+  };
+
+  /**
+   * Response object
+   *
+   * @property {string} status - Whether the request was successful (done) or failed (error)
+   * @property {string} description - Any error or status message
+   * @property {Object} request - The request object
+   * @property {number} [id] - Identifier of inserted object, only available on create statements
+   * @class
+   */
+  mod.Response = function() {
+
+  };
+
+  /**
+   * Execute a HTTP post request and return the resulting promise
+   *
+   * @param {String} actionName Name of action to invoke
+   * @param {Object} requestData  A JSON object with the parameters to send to the web service
+   * @return {Promise.<Object>} a jQuery promise object
+   */
+  mod.doPost = function(actionName,
+    requestData) {
+    return jQuery.post(gsc.dcUrl(), {
+      actionName: actionName,
+      request: JSON.stringify(requestData)
+    }, null, 'json');
+  };
+
+  return mod;
+
+}());
