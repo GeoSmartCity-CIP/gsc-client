208c293313028a9fda59c45704bfa3ae21af0fe7
Adam Kaput <adamkaput@gmail.com>
2016-03-29 11:38:12 +0200

    Updates for jshint

--- test/upload/upload_test.js
+++ test/upload/upload_test.js
@@ -1,53 +1,55 @@
 var gsc = require('../../src/upload/upload');
 // Only for test purposes
 var FileAPI = require('file-api');
-var File = FileAPI.File;
+var FileNode = FileAPI.File;
 
 exports.isGeojsonCorrect = function(test) {
-  var file = new File('test.geojson');
+  var file = new FileNode('test.geojson');
   var dataObject = new gsc.upload.Data(file);
   test.ok(dataObject.isFileTypeCorrect());
   test.done();
 };
 exports.isShpCorrect = function(test) {
-  var file = new File('test.zip');
+  var file = new FileNode('test.zip');
   var dataObject = new gsc.upload.Data(file);
   test.ok(dataObject.isFileTypeCorrect());
   test.done();
 };
 exports.isGmlCorrect = function(test) {
-  var file = new File('test.gml');
+  var file = new FileNode('test.gml');
   var dataObject = new gsc.upload.Data(file);
   test.ok(dataObject.isFileTypeCorrect());
   test.done();
 };
 exports.iskmlCorrect = function(test) {
-  var file = new File('test.kml');
+  var file = new FileNode('test.kml');
   var dataObject = new gsc.upload.Data(file);
   test.ok(dataObject.isFileTypeCorrect());
   test.done();
 };
 exports.iskmlCorrect = function(test) {
-  var file = new File('test.kml');
+  var file = new FileNode('test.kml');
   var dataObject = new gsc.upload.Data(file);
   test.ok(dataObject.isFileTypeCorrect());
   test.done();
 };
 exports.iskmlCorrect = function(test) {
-  var file = new File('test.kml');
+  var file = new FileNode('test.kml');
   var dataObject = new gsc.upload.Data(file);
   test.ok(dataObject.isFileTypeCorrect());
   test.done();
 };
 exports.isEmptyFileFalse = function(test) {
-  var file = new File({name: 'filename', type: 'text/plain', buffer: new Buffer('')});
+  var file = new FileNode({name: 'filename',
+    type: 'text/plain', buffer: new Buffer('')});
   var dataObject = new gsc.upload.Data(file);
   test.strictEqual(dataObject.isFileTypeCorrect(), false);
   test.ok(dataObject.isFileSizeCorrect());
   test.done();
 };
 exports.isTooBigFileFalse = function(test) {
-  var file = new File({name: 'filename', type: 'text/plain', buffer: new Buffer(''), size: 10000000});
+  var file = new FileNode({name: 'filename',
+    type: 'text/plain', buffer: new Buffer(''), size: 10000000});
   var dataObject = new gsc.upload.Data(file);
   test.strictEqual(dataObject.isFileTypeCorrect(), false);
   test.ok(dataObject.isFileSizeCorrect());
