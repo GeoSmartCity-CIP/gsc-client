29968ca9759c7cc51f84388a01f81db1bb6ce495
AdamKaput <adamkaput@gmail.com>
2016-03-31 14:48:59 +0200

    -FileType - generating too many errors, better to check for extension

--- test/upload/upload_test.js
+++ test/upload/upload_test.js
@@ -3,50 +3,6 @@ var gsc = require('../../src/upload/upload');
 var FileAPI = require('file-api');
 var FileNode = FileAPI.File;
 
-exports.isGeojsonCorrect = function(test) {
-  var file = new FileNode('test.geojson');
-  var dataObject = new gsc.upload.Data(file);
-  test.ok(dataObject.isFileTypeCorrect());
-  test.done();
-};
-exports.isShpCorrect = function(test) {
-  var file = new FileNode('test.zip');
-  var dataObject = new gsc.upload.Data(file);
-  test.ok(dataObject.isFileTypeCorrect());
-  test.done();
-};
-exports.isGmlCorrect = function(test) {
-  var file = new FileNode('test.gml');
-  var dataObject = new gsc.upload.Data(file);
-  test.ok(dataObject.isFileTypeCorrect());
-  test.done();
-};
-exports.iskmlCorrect = function(test) {
-  var file = new FileNode('test.kml');
-  var dataObject = new gsc.upload.Data(file);
-  test.ok(dataObject.isFileTypeCorrect());
-  test.done();
-};
-exports.iskmlCorrect = function(test) {
-  var file = new FileNode('test.kml');
-  var dataObject = new gsc.upload.Data(file);
-  test.ok(dataObject.isFileTypeCorrect());
-  test.done();
-};
-exports.iskmlCorrect = function(test) {
-  var file = new FileNode('test.kml');
-  var dataObject = new gsc.upload.Data(file);
-  test.ok(dataObject.isFileTypeCorrect());
-  test.done();
-};
-exports.isEmptyFileFalse = function(test) {
-  var file = new FileNode({name: 'filename',
-    type: 'text/plain', buffer: new Buffer('')});
-  var dataObject = new gsc.upload.Data(file);
-  test.strictEqual(dataObject.isFileTypeCorrect(), false);
-  test.ok(dataObject.isFileSizeCorrect());
-  test.done();
-};
 exports.isTooBigFileFalse = function(test) {
   var file = new FileNode({name: 'filename',
     type: 'text/plain', buffer: new Buffer(''), size: 10000000});
