82ffbb11b109403f8e612a05e25747239a8e4d80
AdamKaput <adamkaput@gmail.com>
2016-03-31 14:49:11 +0200

    -FileType - generating too many errors, better to check for extension

--- test/upload/upload_test.js
+++ test/upload/upload_test.js
@@ -7,7 +7,6 @@ exports.isTooBigFileFalse = function(test) {
   var file = new FileNode({name: 'filename',
     type: 'text/plain', buffer: new Buffer(''), size: 10000000});
   var dataObject = new gsc.upload.Data(file);
-  test.strictEqual(dataObject.isFileTypeCorrect(), false);
   test.ok(dataObject.isFileSizeCorrect());
   test.done();
 };
