76484a8dfb20e7dba8adf9c6d0a9012fa90c8204
Filip Zavadil <filip.zavadil@mapshakers.com>
2016-05-05 09:25:52 +0200

    fix test

--- test/cs/cs.test.js
+++ test/cs/cs.test.js
@@ -1,8 +1,5 @@
 'use strict';
 describe("Testing gsc.cs", function () {
-    it("Contains version", function () {
-        expect(gsc.cs.version !== undefined).toBe(true);
-    });
     it("Has crowdsource URL", function() {
         expect(gsc.cs.csUrl() !== '').toBe(true);
     });
