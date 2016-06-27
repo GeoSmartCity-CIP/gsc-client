ac5f55c0f9742586584a3607463918706bac0d11
Christos <xr_xarmatzis@outlook.com>
2016-06-27 22:39:52 +0300

    first final version

--- src/routing/routing.js
+++ src/routing/routing.js
@@ -4,37 +4,30 @@ var gsc = gsc || {};
 
 gsc.routing = gsc.routing || {};
 
-var RoutingUrl = "http://hub.geosmartcity.eu/GironaRouting/";
-RoutingUrl = "http://localhost:8080/GironaRouting/";
-
 /**
- * Get a list of coordinates for a name
+ * Get a vector layer with the root
  *
- * @param {String[]} geonames An array of one or more geonames
+ * @param {double} x1 coordinate of the start point
+ * @param {double} y1 coordinate of the start point
+ * @param {double} x2 coordinate of the end point
+ * @param {double} y2 coordinate of the end point
  */
-gsc.routing.routing = function (x1, y1, x2, y2) {
-    $.ajax({
-        url: RoutingUrl,
-        type: "GET",
-        dataType: "json",
-        async: false,
-        success: function (data) {
-            var layer1 = new ol.layer.Vector({
-                source: new ol.source.Vector({
-                    url: data,
-                    format: new ol.format.GeoJSON({
-                        defaultDataProjection: 'EPSG:4326',
-                        projection: 'EPSG:3857'
-                    })
-                }),
-                name: 'Route'
-            });
-            return layer1;
-        },
-        error: function (e) {
-            console.log('error: ' + e.result);
-        }
+gsc.routing.routing = function(x1, y1, x2, y2) {
 
-    });
+  //var RoutingUrl = 'http:/hub.geosmartcity.eu'
+  // + '/GironaRouting/geo/RestService/getroute?';
+  var RoutingUrl = 'http://localhost:8080' +
+   '/GironaRouting/geo/RestService/getroute?';
 
+  RoutingUrl += ('x1=' + x1 + '&y1=' + y1 + '&x2=' + x2 + '&y2=' +  y2);
+  console.log(RoutingUrl);
+
+  var	vectorLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      url: RoutingUrl,
+      format: new ol.format.GeoJSON()
+    })
+  });
+  return vectorLayer;
+};
 
