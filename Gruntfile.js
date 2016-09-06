aec349adf02b898403b64208a21e1e142da256fd
Filip Zavadil <dev@filipzavadil.com>
2016-09-06 11:57:44 +0200

    integrate babel and repair tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,169 +1,175 @@
 'use strict';
 
+
 /* jshint node: true */
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %>' +
-            '<%= pkg.author.name %>; Licensed ' +
-            ' <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-        // Task configuration.
-        concat: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                nonull: true,
-                src: [
-                    'node_modules/openlayers/dist/ol.js',
-                    'node_modules/jquery.1/node_modules/jquery/dist/jquery.js',
-                    'node_modules/bootstrap/dist/js/bootstrap.js',
-                    'src/<%= pkg.name %>.js',
-                    'src/**/*.js'
-                ],
-                dest: 'dist/<%= pkg.name %>.js'
-            },
-            gsc: {
-                nonull: true,
-                src: [
-                    'src/<%= pkg.name %>.js',
-                    'src/**/*.js'
-                ],
-                dest: 'dist/<%= pkg.name %>.js'
-            },
-        },
-        jsdoc: {
-            dist: {
-                src: [
-                    'DOC.md',
-                    'dist/*.js'
-                ],
-                options: {
-                    destination: 'doc',
-                    template: 'node_modules/ink-docstrap/template',
-                    configure: 'node_modules/ink-docstrap/template/jsdoc.conf.json'
-                }
-            }
-        },
-        uglify: {
-            options: {
-                banner: '<%= banner %>',
-                preserveComments: false,
-                mangle: {
-                    except: ['jQuery']
-                }
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/<%= pkg.name %>.min.js'
-            },
-        },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-            },
-            all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-            dev: ['src/**/*.js'],
-        },
-        jscs: {
-            src: 'src/**/*.js',
-            options: {
-                preset: 'google.json',
-                config: '.jscsrc',
-                // If you use ES6 http://jscs.info/overview.html#esnext
-                esnext: true,
-                // If you need output with rule names
-                // http://jscs.info/overview.html#verbose
-                verbose: true,
-                // Autofix code style violations when possible.
-                fix: true,
-                requireCurlyBraces: ["if"]
-            }
-        },
-        concat_css: {
-            options: {
-                // Task-specific options go here.
-            },
-            all: {
-                src: [
-                    'node_modules/openlayers/css/ol.css',
-                    'node_modules/bootstrap/dist/css/bootstrap.css',
-                    'node_modules/bootstrap/dist/css/bootstrap-theme.css',
-                    'css/*.css'
-                ],
-                dest: "dist/gsc.css"
-            },
-            gsc: {
-                src: [
-                    'css/*.css'
-                ],
-                dest: "dist/gsc.css"
-            },
-        },
-        cssmin: {
-            target: {
-                files: [{
-                    expand: true,
-                    src: ['dist/gsc.css'],
-                    dest: '',
-                    ext: '.min.css'
-                }]
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib: {
-                files: '<%= jshint.lib.src %>',
-                tasks: ['jshint:src', 'nodeunit']
-            }
-        },
-        jasmine: {
-            customTemplate: {
-                src: 'test/**/*.test.js',
-                options: {
-                    vendor: [
-                        "node_modules/jquery.1/node_modules/jquery/dist/jquery.js",
-                        "node_modules/jquery/dist/jquery.js",
-                        "dist/gsc.js"
-                    ]
-                }
-            }
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    '* Copyright (c) <%= grunt.template.today("yyyy") %>' +
+    '<%= pkg.author.name %>; Licensed ' +
+    ' <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        nonull: true,
+        src: [
+          'node_modules/openlayers/dist/ol.js',
+          'node_modules/jquery/dist/jquery.js',
+          'node_modules/bootstrap/dist/js/bootstrap.js',
+          'src/<%= pkg.name %>.js',
+          'src/**/*.js'
+        ],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      gsc: {
+        nonull: true,
+        src: [
+          'src/<%= pkg.name %>.js',
+          'src/**/*.js'
+        ],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      dist: {
+        files: {
+          'dist/gsc.babel.js': 'dist/gsc.js'
         }
-    });
+      }
+    },
+    jsdoc: {
+      dist: {
+        src: [
+          'DOC.md',
+          'dist/*.js'
+        ],
+        options: {
+          destination: 'doc',
+          template: 'node_modules/ink-docstrap/template',
+          configure: 'node_modules/ink-docstrap/template/jsdoc.conf.json'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>',
+        preserveComments: false,
+        mangle: {
+          except: ['jQuery']
+        }
+      },
+      dist: {
+        src: 'dist/gsc.babel.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      },
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+      },
+      all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      dev: ['src/**/*.js'],
+    },
+    jscs: {
+      src: 'src/**/*.js',
+      options: {
+        preset: 'google.json',
+        config: '.jscsrc',
+        // If you use ES6 http://jscs.info/overview.html#esnext
+        esnext: true,
+        // If you need output with rule names
+        // http://jscs.info/overview.html#verbose
+        verbose: true,
+        // Autofix code style violations when possible.
+        fix: true,
+        requireCurlyBraces: ["if"]
+      }
+    },
+    concat_css: {
+      options: {
+        // Task-specific options go here.
+      },
+      all: {
+        src: [
+          'node_modules/openlayers/css/ol.css',
+          'node_modules/bootstrap/dist/css/bootstrap.css',
+          'node_modules/bootstrap/dist/css/bootstrap-theme.css',
+          'css/*.css'
+        ],
+        dest: "dist/gsc.css"
+      },
+      gsc: {
+        src: [
+          'css/*.css'
+        ],
+        dest: "dist/gsc.css"
+      },
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          src: ['dist/gsc.css'],
+          dest: '',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:src', 'nodeunit']
+      }
+    },
+    jasmine: {
+      customTemplate: {
+        src: 'test/**/*.test.js',
+        options: {
+          vendor: [
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/openlayers/dist/ol.js',
+            "dist/gsc.min.js"
+          ]
+        }
+      }
+    }
+  });
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks("grunt-jscs");
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jsdoc');
-    grunt.loadNpmTasks('grunt-concat-css');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-    // Default task.
-    grunt.registerTask('default',
-        ['jshint', 'jscs', 'build-js', 'build-css', 'update-docs']);
+  // Default task.
+  grunt.registerTask('default',
+    ['jshint', 'jscs', 'build-js', 'build-css', 'update-docs']);
 
-    grunt.registerTask('build-js', ['concat', 'uglify']);
-    grunt.registerTask('build-css', ['concat_css', 'cssmin']);
-    grunt.registerTask('update-docs', ['concat', 'jsdoc']);
 
-    // Default task - only GSC custom code.
-    grunt.registerTask('default-gsc', ['jshint', 'jscs', 'build-js-gsc', 'build-css-gsc', 'update-docs-gsc']);
-    grunt.registerTask('update-docs-gsc', ['concat:gsc', 'jsdoc']);
-    grunt.registerTask('build-js-gsc', ['concat:gsc', 'uglify']);
-    grunt.registerTask('build-css-gsc', ['concat_css:gsc', 'cssmin']);
-    grunt.registerTask('update-docs-gsc', ['concat:gsc', 'jsdoc']);
+  grunt.registerTask('babeling',['babel']);
+
+  grunt.registerTask('build-js', ['concat','babel', 'uglify']);
+  grunt.registerTask('build-css', ['concat_css', 'cssmin']);
+  grunt.registerTask('update-docs', ['concat', 'jsdoc']);
+
+  // Default task - only GSC custom code.
+  grunt.registerTask('default-gsc', ['jshint', 'jscs', 'build-js-gsc', 'build-css-gsc', 'update-docs-gsc']);
+  grunt.registerTask('update-docs-gsc', ['concat:gsc', 'jsdoc']);
+  grunt.registerTask('build-js-gsc', ['concat:gsc', 'uglify']);
+  grunt.registerTask('build-css-gsc', ['concat_css:gsc', 'cssmin']);
+  grunt.registerTask('update-docs-gsc', ['concat:gsc', 'jsdoc']);
 };
--- test/upload/upload.test.js
+++ test/upload/upload.test.js
@@ -1,15 +1,8 @@
-/* global File */
 'use strict';
 
 describe('Testing gsc.upload', function () {
   it("Contains Data method", function () {
-    expect(gsc.map.hasOwnProperty('Data')).toBe(true);
+    expect(gsc.upload.hasOwnProperty('Data')).toBe(true);
   });
-  
-  it('Checks file size', function() {
-    var file = new File([''], 'filename',
-        {type: 'text/plain', size: 10000000});
-    var dataObject = new gsc.upload.Data(file);
-    expect(dataObject.isFileSizeCorrect()).toBe(true);
-  });
+
 });
